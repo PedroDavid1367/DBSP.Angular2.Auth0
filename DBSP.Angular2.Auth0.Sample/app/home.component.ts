06aab2bc2a4ef56d084294605dcf01fb786791f7
David1367 <pedro.571.david@gmail.com>
2016-07-21 16:19:32 -0400

    Initial project

---
+++ DBSP.Angular2.Auth0.Sample/app/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home',
+  template: `
+    <h1>Welcome to auth0 - Angular 2 integration app</h1>
+  `
+})
+
+export class HomeComponent {}
